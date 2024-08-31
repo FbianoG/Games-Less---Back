6052f5a990ad0aaa1e9fbf47aadcc9e07122ccdf
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-08-31 20:51:07 -0300

    ,

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express'
 import router from './routes/router'
 import cors from 'cors'
-import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
-app.use(cookieParser())
+app.use(cors({
+    origin: 'https://game-less.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
